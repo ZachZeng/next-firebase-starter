b93dfdf984598220828a933b3fff05ac4f8014ee
ZachZeng <zachzeng@umich.edu>
2021-06-03 14:23:28 -0700

    style login and register page

--- pages/users/login.js
+++ pages/users/login.js
@@ -5,7 +5,19 @@ import Link from "next/link";
 
 import { useAuth } from "../../context/AuthUserContext";
 
-import { Form, FormInput, FormGroup, Alert } from "shards-react";
+import { Form, FormInput, FormGroup, Alert, Button } from "shards-react";
+
+import styled from "styled-components";
+
+const LoginWrapper = styled.div`
+	width: 70vw;
+	margin: 10rem auto;
+`;
+
+const FormWrapper = styled.div`
+	width: 30rem;
+	margin-top: 3rem;
+`;
 
 const Login = () => {
 	const [username, setUsername] = useState("");
@@ -29,39 +41,46 @@ const Login = () => {
 	};
 
 	return (
-		<div>
-			<h2>Login</h2>
-			<Alert
-				theme="danger"
-				dismissible={() => setNotifyVisible(false)}
-				open={notifyVisible}
-			>
-				{notify}
-			</Alert>
-
-			<Form onSubmit={handleLogin}>
-				<FormGroup>
-					<label htmlFor="#username">Email</label>
-					<FormInput
-						type="text"
-						placeholder="username"
-						value={username}
-						onChange={({ target }) => setUsername(target.value)}
-					/>
-				</FormGroup>
-				<FormGroup>
-					<label htmlFor="#password">Password</label>
-					<FormInput
-						type="password"
-						placeholder="username"
-						value={password}
-						onChange={({ target }) => setPassword(target.value)}
-					/>
-				</FormGroup>
-				<button type="submit">Login</button>
-				No account? <Link href="/users/register">Create one</Link>
-			</Form>
-		</div>
+		<LoginWrapper>
+			<h1>Login</h1>
+			<FormWrapper>
+				<Form onSubmit={handleLogin}>
+					<Alert
+						theme="danger"
+						dismissible={() => setNotifyVisible(false)}
+						open={notifyVisible}
+						style={{ marginTop: "3rem" }}
+					>
+						{notify}
+					</Alert>
+					<FormGroup>
+						<label htmlFor="#username">Email</label>
+						<FormInput
+							type="text"
+							placeholder="username"
+							value={username}
+							onChange={({ target }) => setUsername(target.value)}
+						/>
+					</FormGroup>
+					<FormGroup>
+						<label htmlFor="#password">Password</label>
+						<FormInput
+							type="password"
+							placeholder="username"
+							value={password}
+							onChange={({ target }) => setPassword(target.value)}
+						/>
+					</FormGroup>
+					<Button type="submit" style={{ marginTop: "1rem" }}>
+						Login
+					</Button>
+					<div style={{ marginTop: "1rem" }}>
+						No account?{" "}
+						<Link href="/users/register">Create one</Link>
+					</div>
+				</Form>
+			</FormWrapper>
+		</LoginWrapper>
 	);
 };
 export default Login;
--- pages/users/register.js
+++ pages/users/register.js
@@ -4,12 +4,27 @@ import { useRouter } from "next/router";
 import { useAuth } from "../../context/AuthUserContext";
 import Link from "next/link";
 
+import { Form, FormInput, FormGroup, Alert, Button } from "shards-react";
+
+import styled from "styled-components";
+
+const SignupWrapper = styled.div`
+	width: 70vw;
+	margin: 10rem auto;
+`;
+
+const FormWrapper = styled.div`
+	width: 30rem;
+	margin-top: 3rem;
+`;
+
 const Register = () => {
 	const router = useRouter();
 	const [userName, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 	const [passConf, setPassConf] = useState("");
 	const [notification, setNotification] = useState("");
+	const [notifyVisible, setNotifyVisible] = useState(false);
 
 	const { createUserWithEmailAndPassword } = useAuth();
 
@@ -19,11 +34,7 @@ const Register = () => {
 			setNotification(
 				"Password and password confirmation does not match"
 			);
-			setTimeout(() => {
-				setNotification("");
-			}, 2000);
-			setPassword("");
-			setPassConf("");
+			setNotifyVisible(true);
 			return null;
 		}
 
@@ -35,38 +46,59 @@ const Register = () => {
 			.catch((error) => {
 				// An error occurred. Set error message to be displayed to user
 				setNotification(error.message);
+				setNotifyVisible(true);
 			});
 	};
 	return (
-		<div>
+		<SignupWrapper>
 			<h1>Create new user</h1>
-			{notification}
-			<form onSubmit={handleLogin}>
-				Email:{" "}
-				<input
-					type="text"
-					value={userName}
-					onChange={({ target }) => setUsername(target.value)}
-				/>
-				<br />
-				Password:{" "}
-				<input
-					type="password"
-					value={password}
-					onChange={({ target }) => setPassword(target.value)}
-				/>
-				<br />
-				Password conf:{" "}
-				<input
-					type="password"
-					value={passConf}
-					onChange={({ target }) => setPassConf(target.value)}
-				/>
-				<br />
-				<button type="submit">Signup</button>
-				Already have an account? <Link href="/users/login">Login</Link>
-			</form>
-		</div>
+
+			<FormWrapper>
+				<Form onSubmit={handleLogin}>
+					<Alert
+						theme="danger"
+						dismissible={() => setNotifyVisible(false)}
+						open={notifyVisible}
+					>
+						{notification}
+					</Alert>
+					<FormGroup>
+						<label htmlFor="#username">Email</label>
+						<FormInput
+							type="text"
+							placeholder="username"
+							value={userName}
+							onChange={({ target }) => setUsername(target.value)}
+						/>
+					</FormGroup>
+					<FormGroup>
+						<label htmlFor="#password">Password</label>
+						<FormInput
+							type="password"
+							placeholder="username"
+							value={password}
+							onChange={({ target }) => setPassword(target.value)}
+						/>
+					</FormGroup>
+					<FormGroup>
+						<label htmlFor="#passwordconf">Password</label>
+						<FormInput
+							type="password"
+							placeholder="password confirmation"
+							value={passConf}
+							onChange={({ target }) => setPassConf(target.value)}
+						/>
+					</FormGroup>
+					<Button type="submit" style={{ marginTop: "1rem" }}>
+						Signup
+					</Button>
+					<div style={{ marginTop: "1rem" }}>
+						Already have an account?{" "}
+						<Link href="/users/login">Login</Link>
+					</div>
+				</Form>
+			</FormWrapper>
+		</SignupWrapper>
 	);
 };
 export default Register;
